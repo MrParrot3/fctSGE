acc0f88308c6ecf1f6c791a0812252f61589c3fa
ubuntu <ubuntu@ubuntu>
2018-01-31 14:20:30 +0100

    modulo SGE miercoles 31 parte 3

--- MyOdooModules/fct/models/models.py
+++ MyOdooModules/fct/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class Activity(models.Model):
     _name = 'fct.activity'
@@ -10,7 +10,11 @@ class Activity(models.Model):
     duration = fields.Float()
     remarks = fields.Char(string="Remarks")
     
-    
+    @api.constrains('duration')
+    def _verify_valid_duration(self):
+        for r in self:
+            if r.duration > 8:
+                raise exceptions.ValidationError("A duration can't be more than 8 hours")
     
     
 
